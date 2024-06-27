b6a8b6e3ded6b53eee8ec9813cd3a2bd099363f0
akshay kumar gupta <guptaakshay0409@gmail.com>
2024-06-27 15:29:10 +0530

    basics of opps

---
+++ Basic-opps.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+class Person {
+    public : 
+
+
+    private : 
+    // member variable naming conventon ;
+    int a_ ;
+    string name_ ;
+    string work_address_ ;
+    float cgpa_ ;
+
+};
+
+int main(){
+      
+      
+
+
+
+    cout << "akshay" << '\n';
+    return 0;
+}
